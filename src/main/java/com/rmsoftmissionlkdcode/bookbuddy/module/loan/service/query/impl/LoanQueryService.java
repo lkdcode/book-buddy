b078c8e5a57db851ec1c2323957209fdbab19e2e
lkdcode <dlrlejr1@naver.com>
2023-11-23 19:27:24 +0900

    Add [LoanQueryService] : initial commit

---
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/service/query/impl/LoanQueryService.java
@@ -0,0 +1,12 @@
+package com.rmsoftmissionlkdcode.bookbuddy.module.loan.service.query.impl;
+
+import com.rmsoftmissionlkdcode.bookbuddy.module.loan.service.query.LoanQueryUsecase;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanQueryService implements LoanQueryUsecase {
+    @Override
+    public void retrieveFindAllByBookId(Long bookId) {
+
+    }
+}
