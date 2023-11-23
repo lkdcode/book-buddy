7864cd6538fd70622e59e85ebe260e22d6bccd06
lkdcode <dlrlejr1@naver.com>
2023-11-23 19:24:12 +0900

    Add [Loan] : Loan

---
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/domain/Loan.java
@@ -0,0 +1,53 @@
+package com.rmsoftmissionlkdcode.bookbuddy.module.loan.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.rmsoftmissionlkdcode.bookbuddy.module.book.domain.Book;
+import com.rmsoftmissionlkdcode.bookbuddy.module.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
+
+@Entity
+@Table(name = "tb_loan")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "borrowed_at", updatable = false)
+    @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+    private LocalDateTime borrowedAt;
+
+    @Column(name = "returned_at")
+    @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+    private LocalDateTime returnedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Builder
+    public Loan(User user, Book book) {
+        this.user = user;
+        this.book = book;
+    }
+
+    public void returned() {
+        this.returnedAt = LocalDateTime.now();
+    }
+}
