1bbb0db67417141012143a2e85c1d553b1b16d42
lkdcode <dlrlejr1@naver.com>
2023-11-23 19:17:25 +0900

    Add [BaseEntity] : BaseEntity

---
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/global/common/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.rmsoftmissionlkdcode.bookbuddy.global.common;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+    private LocalDateTime updatedAt;
+}
