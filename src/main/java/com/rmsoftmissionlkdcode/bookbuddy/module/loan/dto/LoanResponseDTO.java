310ec1ab9ffc32c9e09b449fa66780bdce220bda
lkdcode <dlrlejr1@naver.com>
2023-11-24 20:10:24 +0900

    Add [build.LoanResponseDTO] : Loan, Returned

--- src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/dto/LoanResponseDTO.java
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/dto/LoanResponseDTO.java
@@ -1,16 +1,34 @@
 package com.rmsoftmissionlkdcode.bookbuddy.module.loan.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
 import static com.rmsoftmissionlkdcode.bookbuddy.module.loan.dto.LoanResponseDTO.Loan;
 import static com.rmsoftmissionlkdcode.bookbuddy.module.loan.dto.LoanResponseDTO.Returned;
 
 public sealed interface LoanResponseDTO permits Loan, Returned {
+    @Builder
     record Loan(
-            boolean success
+            String userEmail,
+            String title,
+            String author,
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+            LocalDateTime borrowAt
     ) implements LoanResponseDTO {
     }
 
+    @Builder
     record Returned(
-            boolean success
+            String userEmail,
+            String title,
+            String author,
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+            LocalDateTime borrowAt,
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+            LocalDateTime returnedAt
     ) implements LoanResponseDTO {
     }
 }
