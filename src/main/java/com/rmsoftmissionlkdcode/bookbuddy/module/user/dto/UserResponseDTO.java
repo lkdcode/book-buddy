15f97e1bc1dee016302578fa41193f79ae205726
lkdcode <dlrlejr1@naver.com>
2023-11-25 20:24:11 +0900

    Add [UserResponseDTO] : Builder

--- src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/user/dto/UserResponseDTO.java
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/user/dto/UserResponseDTO.java
@@ -1,13 +1,17 @@
 package com.rmsoftmissionlkdcode.bookbuddy.module.user.dto;
 
+import lombok.Builder;
+
 import static com.rmsoftmissionlkdcode.bookbuddy.module.user.dto.UserResponseDTO.*;
 
 public sealed interface UserResponseDTO permits SignUp, SignIn {
+    @Builder
     record SignUp(
             String email
     ) implements UserResponseDTO {
     }
 
+    @Builder
     record SignIn(
             String jwt
     ) implements UserResponseDTO {
