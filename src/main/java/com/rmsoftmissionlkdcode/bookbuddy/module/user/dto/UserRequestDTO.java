6257341415779243ce3af8d5d7a055d5c7d1f555
lkdcode <dlrlejr1@naver.com>
2023-11-25 20:24:21 +0900

    Add [UserRequestDTO] : Builder

--- src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/user/dto/UserRequestDTO.java
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/user/dto/UserRequestDTO.java
@@ -1,11 +1,14 @@
 package com.rmsoftmissionlkdcode.bookbuddy.module.user.dto;
 
+import lombok.Builder;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 import static com.rmsoftmissionlkdcode.bookbuddy.module.user.dto.UserRequestDTO.*;
 
 public sealed interface UserRequestDTO permits SignUp {
+    @Builder
     record SignUp(
             @NotBlank(message = "이메일을 입력해주세요.")
             @Email(message = "올바른 이메일 형식이 아닙니다.")
