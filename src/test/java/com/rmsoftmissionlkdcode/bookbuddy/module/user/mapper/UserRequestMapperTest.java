fff203fbe8ea22dd0d72eee967346ade13263b54
lkdcode <dlrlejr1@naver.com>
2023-11-25 20:23:57 +0900

    Refactor [UserRequestMapperTest] : 더미 데이터를 사용하도록 변경

--- src/test/java/com/rmsoftmissionlkdcode/bookbuddy/module/user/mapper/UserRequestMapperTest.java
+++ src/test/java/com/rmsoftmissionlkdcode/bookbuddy/module/user/mapper/UserRequestMapperTest.java
@@ -2,35 +2,56 @@ package com.rmsoftmissionlkdcode.bookbuddy.module.user.mapper;
 
 import com.rmsoftmissionlkdcode.bookbuddy.module.user.domain.User;
 import com.rmsoftmissionlkdcode.bookbuddy.module.user.dto.UserRequestDTO;
-import org.junit.jupiter.api.BeforeEach;
+import com.rmsoftmissionlkdcode.bookbuddy.support.base.BaseRepositoryList;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class UserRequestMapperTest {
-    private static final String EMAIL = "test@test.com";
-    private static final String PASSWORD = "password123";
-    private static final String NAME = "홍길동";
+class UserRequestMapperTest extends BaseRepositoryList {
 
     @Test
-    @DisplayName("signUpDTO를 User Entity로 변환에 성공할 것이다.")
-    void convert_sign_up_DTO_To_User_Success() {
+    @DisplayName("유효한 signUpDTO를 User Entity로 변환에 성공할 것이다.")
+    void convertValidSignUpDTOToUserSuccessTest() {
         // given
-        UserRequestDTO.SignUp signUpDTO = new UserRequestDTO.SignUp(EMAIL, PASSWORD, NAME);
+        UserRequestDTO.SignUp validSignUpDTO = UserRequestDTO.SignUp.builder()
+                .email(USER_EMAIL)
+                .password(USER_PASSWORD)
+                .name(USER_NAME)
+                .build();
 
         // when
-        User user = UserRequestMapper.INSTANCE.signUpDTOToUser(signUpDTO);
+        User user = UserRequestMapper.INSTANCE.signUpDTOToUser(validSignUpDTO);
 
         // then
         assertThat(user.getEmail())
-                .isEqualTo(EMAIL);
+                .isEqualTo(USER_EMAIL);
 
         assertThat(user.getPassword())
-                .isEqualTo(PASSWORD);
+                .isEqualTo(USER_PASSWORD);
 
         assertThat(user.getName())
-                .isEqualTo(NAME);
+                .isEqualTo(USER_NAME);
     }
 
+    @Test
+    @DisplayName("유효한 signUpDTO를 User Entity로 변환에 성공할 것이다.")
+    void convertInvalidSignUpDTOToUserSuccessTest() {
+        // given
+        UserRequestDTO.SignUp invalidSignUpDTO = UserRequestDTO.SignUp.builder()
+                .build();
+
+        // when
+        User user = UserRequestMapper.INSTANCE.signUpDTOToUser(invalidSignUpDTO);
+
+        // then
+        assertThat(user.getEmail())
+                .isNull();
+
+        assertThat(user.getPassword())
+                .isNull();
+
+        assertThat(user.getName())
+                .isNull();
+    }
 }
