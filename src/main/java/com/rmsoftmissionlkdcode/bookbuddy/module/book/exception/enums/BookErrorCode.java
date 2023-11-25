0b776bd58b9fb58d1f6b2a0bb1cb49a0de031cb8
lkdcode <dlrlejr1@naver.com>
2023-11-25 20:26:48 +0900

    Add [BookErrorCode] : INSUFFICIENT_BOOK_QUANTITY_ERROR, NEGATIVE_BOOK_QUANTITY_NOT_ALLOWED_ERROR

--- src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/book/exception/enums/BookErrorCode.java
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/book/exception/enums/BookErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum BookErrorCode implements AppErrorCode {
     BOOK_ISBN_DUPLICATION_ERROR("이미 등록되어 있는 책입니다.", HttpStatus.BAD_REQUEST),
     NOT_FOUND_BOOK_BY_ID_ERROR("존재하지 않는 도서입니다.", HttpStatus.BAD_REQUEST),
-    NO_CHANGES_TO_APPLY_ERROR("아이디 혹은 비밀번호를 다시 확인해주세요.", HttpStatus.BAD_REQUEST),
+    NEGATIVE_BOOK_QUANTITY_NOT_ALLOWED_ERROR("도서의 최소 수량은 0권 이상입니다.", HttpStatus.BAD_REQUEST),
+    INSUFFICIENT_BOOK_QUANTITY_ERROR("빌릴 수 있는 도서가 없습니다.", HttpStatus.BAD_REQUEST),
     ;
 
     private final String message;
