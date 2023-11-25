676efdbb0d5e31cecfe3e8a8a5d0ac3470851244
lkdcode <dlrlejr1@naver.com>
2023-11-26 00:49:52 +0900

    Refactor [LoanCommandUsecase] : DTO 네이밍 수정

--- src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/service/command/LoanCommandUsecase.java
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/service/command/LoanCommandUsecase.java
@@ -4,7 +4,7 @@ import com.rmsoftmissionlkdcode.bookbuddy.module.loan.dto.LoanRequestDTO;
 import com.rmsoftmissionlkdcode.bookbuddy.module.loan.dto.LoanResponseDTO;
 
 public interface LoanCommandUsecase {
-    LoanResponseDTO.Loan executeLoanBookToUser(Long bookId, LoanRequestDTO.Loan dto);
+    LoanResponseDTO.Borrowed executeLoanBookToUser(Long bookId, LoanRequestDTO.CheckOutDTO dto);
 
-    LoanResponseDTO.Returned executeReturnBook(Long loanId, Long bookId, LoanRequestDTO.Returned dto);
+    LoanResponseDTO.Returned executeReturnBook(Long loanId, Long bookId, LoanRequestDTO.CheckInDTO dto);
 }
