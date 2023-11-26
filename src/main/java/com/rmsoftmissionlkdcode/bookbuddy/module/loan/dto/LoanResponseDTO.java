692a6d82d8f433835f3ad8932067264bbbe4be7d
lkdcode <dlrlejr1@naver.com>
2023-11-26 17:42:15 +0900

    Add [LoanResponseDTO] : Add HistoryDTO

--- src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/dto/LoanResponseDTO.java
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/dto/LoanResponseDTO.java
@@ -6,10 +6,9 @@ import lombok.Builder;
 import java.time.LocalDateTime;
 
 import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
-import static com.rmsoftmissionlkdcode.bookbuddy.module.loan.dto.LoanResponseDTO.Borrowed;
-import static com.rmsoftmissionlkdcode.bookbuddy.module.loan.dto.LoanResponseDTO.Returned;
+import static com.rmsoftmissionlkdcode.bookbuddy.module.loan.dto.LoanResponseDTO.*;
 
-public sealed interface LoanResponseDTO permits Borrowed, Returned {
+public sealed interface LoanResponseDTO permits Borrowed, Returned, History {
     @Builder
     record Borrowed(
             Long loanId,
@@ -23,6 +22,19 @@ public sealed interface LoanResponseDTO permits Borrowed, Returned {
 
     @Builder
     record Returned(
+            String userEmail,
+            String title,
+            String author,
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+            LocalDateTime borrowedAt,
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+            LocalDateTime returnedAt
+    ) implements LoanResponseDTO {
+    }
+
+    @Builder
+    record History(
+            Long loanId,
             String userEmail,
             String title,
             String author,
