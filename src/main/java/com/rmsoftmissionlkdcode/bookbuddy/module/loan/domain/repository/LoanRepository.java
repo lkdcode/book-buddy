4434c97ebe4689a354383aacb0cf6f702182698a
lkdcode <dlrlejr1@naver.com>
2023-11-26 17:42:36 +0900

    Add [LoanRepository] : findByBookId method

--- src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/domain/repository/LoanRepository.java
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/domain/repository/LoanRepository.java
@@ -4,7 +4,11 @@ import com.rmsoftmissionlkdcode.bookbuddy.module.loan.domain.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    List<Loan> findByBookId(Long bookId);
+
     boolean existsById(Long id);
 }
