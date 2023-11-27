38113d9ce0ade2a39aefd9397dd587817c92b9e7
lkdcode <dlrlejr1@naver.com>
2023-11-28 05:43:54 +0900

    Add [LoanRepository] : findByIdForUpdate

--- src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/domain/repository/LoanRepository.java
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/domain/repository/LoanRepository.java
@@ -2,12 +2,23 @@ package com.rmsoftmissionlkdcode.bookbuddy.module.loan.domain.repository;
 
 import com.rmsoftmissionlkdcode.bookbuddy.module.loan.domain.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select l " +
+            "from Loan l " +
+            "where l.id = :id")
+    Optional<Loan> findByIdForUpdate(@Param("id") Long loanId);
+
     List<Loan> findByBookId(Long bookId);
 
     boolean existsById(Long id);
