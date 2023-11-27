d0fa5df579f39635f4d0eb7f8ba9c1191dc24bcc
lkdcode <dlrlejr1@naver.com>
2023-11-27 23:09:36 +0900

    Add [BookErrorCode] : INVALID_ISBN_LENGTH_ERROR

--- src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/book/exception/enums/BookErrorCode.java
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/book/exception/enums/BookErrorCode.java
@@ -13,6 +13,7 @@ public enum BookErrorCode implements AppErrorCode {
     NEGATIVE_BOOK_QUANTITY_NOT_ALLOWED_ERROR("도서의 최소 수량은 0권 이상입니다.", HttpStatus.BAD_REQUEST),
     INSUFFICIENT_BOOK_QUANTITY_ERROR("빌릴 수 있는 도서가 없습니다.", HttpStatus.NOT_FOUND),
     NO_CHANGE_BOOK_INFORMATION("수정된 내용이 없습니다.", HttpStatus.BAD_REQUEST),
+    INVALID_ISBN_LENGTH_ERROR("ISBN은 10자리 혹인 13자리 입니다.", HttpStatus.BAD_REQUEST),
     ;
 
     private final String message;
