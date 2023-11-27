2e2d656236aff8ff3e9178198fbeb8e4ec151f59
lkdcode <dlrlejr1@naver.com>
2023-11-27 09:31:40 +0900

    Add [LoanQueryService] : LoanQueryService

--- src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/service/query/impl/LoanQueryService.java
+++ src/main/java/com/rmsoftmissionlkdcode/bookbuddy/module/loan/service/query/impl/LoanQueryService.java
@@ -1,12 +1,26 @@
 package com.rmsoftmissionlkdcode.bookbuddy.module.loan.service.query.impl;
 
+import com.rmsoftmissionlkdcode.bookbuddy.module.loan.domain.Loan;
+import com.rmsoftmissionlkdcode.bookbuddy.module.loan.domain.repository.LoanRepository;
+import com.rmsoftmissionlkdcode.bookbuddy.module.loan.dto.LoanResponseDTO;
+import com.rmsoftmissionlkdcode.bookbuddy.module.loan.mapper.LoanResponseMapper;
 import com.rmsoftmissionlkdcode.bookbuddy.module.loan.service.query.LoanQueryUsecase;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class LoanQueryService implements LoanQueryUsecase {
-    @Override
-    public void retrieveFindAllByBookId(Long bookId) {
+    private final LoanRepository loanRepository;
 
+    @Override
+    public List<LoanResponseDTO.History> retrieveFindAllByBookId(Long bookId) {
+        List<Loan> loanList = loanRepository.findByBookId(bookId);
+
+        return LoanResponseMapper.INSTANCE.loanToHistoryListDTO(loanList);
     }
 }
