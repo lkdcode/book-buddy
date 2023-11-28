9918377d5116fe555491272086f0cd2820fc4943
lkdcode <dlrlejr1@naver.com>
2023-11-29 04:48:52 +0900

    Refactor [UserRegisterServiceTest] : delete Transactional

--- src/test/java/com/rmsoftmissionlkdcode/bookbuddy/module/user/service/command/impl/UserRegisterServiceTest.java
+++ src/test/java/com/rmsoftmissionlkdcode/bookbuddy/module/user/service/command/impl/UserRegisterServiceTest.java
@@ -17,7 +17,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
-@Transactional
 class UserRegisterServiceTest {
     @Autowired
     private UserRegisterService userRegisterService;
